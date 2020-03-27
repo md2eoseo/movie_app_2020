aed5f6f21a1bf0cdea15c4e2a204ed2435f7c9e0
md2eoseo <md2eoseo@gmail.com>
2020-03-27 15:15:26 +0100

    simple styling

--- src/Movie.js
+++ src/Movie.js
@@ -7,8 +7,10 @@ function Movie({ year, title, summary, poster, genres }) {
     <div className="movie">
       <img className="movie_poster" src={poster} alt={title} title={title} />
       <div className="movie_data">
-        <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
+        <h2 className="movie_title">
+          {title} (<span className="movie_year">{year}</span>)
+        </h2>
+
         <ul className="genres">
           {genres.map((genre, index) => (
             <li key={index} className="genres_genre">
