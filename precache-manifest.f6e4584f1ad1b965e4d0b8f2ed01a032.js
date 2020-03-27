c12f6ef5578e799ce1d919849a61e561e91e0cb5
md2eoseo <md2eoseo@gmail.com>
2020-03-27 15:31:13 +0100

    Updates

---
+++ precache-manifest.f6e4584f1ad1b965e4d0b8f2ed01a032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b581736a367f5f4492b7e55486a5736",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "2e58ada5ad28b82d0ced",
+    "url": "/movie_app_2020/static/css/main.e1b20473.chunk.css"
+  },
+  {
+    "revision": "ee5f40fffa95eb75bff2",
+    "url": "/movie_app_2020/static/js/2.41708d2b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2020/static/js/2.41708d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e58ada5ad28b82d0ced",
+    "url": "/movie_app_2020/static/js/main.9c50884b.chunk.js"
+  },
+  {
+    "revision": "5b9de689d76c950937df",
+    "url": "/movie_app_2020/static/js/runtime-main.db2dcdc9.js"
+  }
+]);
